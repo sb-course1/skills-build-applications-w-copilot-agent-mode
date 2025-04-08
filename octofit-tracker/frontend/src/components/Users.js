58a821d44704947f2efd0cb0d7963e4c31227b59
sb-course1 <s.betzler+coursera@gmail.com>
2025-04-08 22:11:27 +0000

    Style Octofit Tracker frontend with Bootstrap and add logo and favicon

---
+++ octofit-tracker/frontend/src/components/Users.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://automatic-trout-r464qj4xwx4635v9p-8000.app.github.dev/api/users/')
+      .then(response => response.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  return (
+    <div>
+      <h1 className="mb-4">Users</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user._id}>
+              <td>{user.username}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Users;
